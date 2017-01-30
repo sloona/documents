dc0f5e52f5a42ce5d22444a4b8f4ed8af26e998d
sloona <sloona@mail.ru>
2017-01-30 20:08:23 +0400

    After deleted(upload and download file)

---
+++ DocumentSystem/ModelDomain/Services/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Services
+{
+    public interface IEntityRepository<T>
+        where T : class
+    {
+
+        void Update(T operResult);
+
+        T Create();
+
+        IEnumerable<T> GetAll();
+
+        //T Load(int d);
+
+        //bool Delete(int Id);
+    }
+}
---
+++ DocumentSystem/Web/Models/DocumentModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class DocumentModel
+    {
+        [DisplayName("Название")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Файл")]
+        [Required]
+        public HttpPostedFileBase Attachment { get; set; }
+
+        public object[] GetParametrs()
+        {
+            return new object[] { Name };
+        }
+    }
+}
